28d8c4d65c5294628ce35950c8d057b4b696784e
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-01-14 21:19:15 -0500

    Se agrega archivo test.html y correccion archivo test.js (id txtNombre)- funcional hasta saludar en consola

--- trabajo/test.js
+++ trabajo/test.js
@@ -1,5 +1,5 @@
 function saludar(){
-    const nombre = document.getElementById('txtnombre').value;
+    const nombre = document.getElementById('txtNombre').value;
     const apellido = document.getElementById('txtApellido').value;
 
     const saludoProcesado = generarSaludo(nombre, apellido);
