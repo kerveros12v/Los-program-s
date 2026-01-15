4cc2814f9825a82ede959221d999eea2db8042a7
Vinueza Pozo Gabriel Fernando <47395367+kerveros12v@users.noreply.github.com>
2026-01-14 21:33:22 -0500

    Actualizar test.js
    agregando funcionalidad a h4 id resultado

--- trabajo/test.js
+++ trabajo/test.js
@@ -3,6 +3,7 @@ function saludar(){
     const apellido = document.getElementById('txtApellido').value;
 
     const saludoProcesado = generarSaludo(nombre, apellido);
-
+let resultado=document.getElementById('resultado);
+resultado.innerHTML=saludoProcesado;
     console.log(saludoProcesado);
 }
